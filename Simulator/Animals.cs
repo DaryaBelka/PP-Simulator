3d0e530a21e03f116a4f1c9f5add8aef88a5ab8a
Darya <225284@student.uek.krakow.pl>
2024-10-28 14:28:41 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,16 @@
+﻿namespace Simulator;
+
+public class Animals
+{
+    public required string Description { get; init; }
+    public uint Size { get; set; } = 3;
+
+    //public string Info
+    //{
+    //    get
+    //    {
+    //        return Description + " <" + Size + ">";
+    //    }
+    //}
+    public string Info => $"{Description} <{Size}>"; //np. Dogs <3>
+}
