4c9de4e4f0c50f91ac9ac9d3b708d243aced7e42
Darya <225284@student.uek.krakow.pl>
2024-10-28 15:47:55 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -2,8 +2,35 @@
 
 public class Animals
 {
-    public required string Description { get; init; }
     public uint Size { get; set; } = 3;
+    private string _description = "Unknown";
+    public required string Description
+
+    {
+        get { return _description; }
+        init
+        {
+            if (value == null)
+            {
+                _description = "Unknown";
+                return;
+            }
+
+            value = value.Trim();
+            if (value.Length < 3)
+                value = value.PadRight(3, '#');
+
+            if (value.Length > 15)
+                value = value.Substring(0, 15).TrimEnd();
+
+            if (char.IsLower(value[0]))
+                value = char.ToUpper(value[0]) + value.Substring(1); ;
+
+
+            _description = value;
+        }
+    }
+
 
     //public string Info
     //{
@@ -12,5 +39,6 @@ public class Animals
     //        return Description + " <" + Size + ">";
     //    }
     //}
+
     public string Info => $"{Description} <{Size}>"; //np. Dogs <3>
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,19 +1,51 @@
-﻿using System.Reflection.Emit;
-using System.Xml.Linq;
-
-namespace Simulator;
+﻿namespace Simulator;
 
 public class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
+    private string _name = "Unknown";
+    private int _level = 1;
+    public string Name { 
+        get { return _name; }
+        init { 
+            if (value == null)
+            {
+                _name = "Unknown";
+                return;
+            }
+            
+            value = value.Trim(); // Usuń nadmiarowe spacje na początku i końcu
 
-    public Creature(string name, int level = 1)
-    {
-    Name = name; Level = level; 
+            if (value.Length < 3)
+                value = value.PadRight(3, '#');
+
+            if (value.Length > 25)
+                value = value.Substring(0, 25).TrimEnd();
+ 
+            if (char.IsLower(value[0]))
+                value = char.ToUpper(value[0]) + value.Substring(1); ;
+           
+            _name = value;
+        }
     }
 
-    public Creature() { }
+    public int Level {
+        get { return _level; }
+        init
+        {
+            if (value < 1) _level = 1;
+            else if (value > 10) _level = 10;
+            else _level = value;
+
+      }
+    }
+    
+    public Creature(string name, int level = 1)
+    {
+    Name = name; Level = level;
+}
+
+    public Creature() {
+    }
 
     public void SayHi()
     {
@@ -23,5 +55,12 @@ public class Creature
     public string Info
     {
         get { return $"{Name} [{Level}]"; }
+    }   
+    
+    public void Upgrade()
+    { 
+        if (Level < 10 )
+            _level ++;    
+      
     }
 }
