c7e92858675af7a3c23c6ed6cb5a0a2eadbe9c2d
Darya <225284@student.uek.krakow.pl>
2024-11-09 15:18:01 +0100

    Prostokąty

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -4,58 +4,35 @@ class Program
 {
     static void Main()
     {
-        Lab4a();
-        Lab4b();
+        Lab5a();
     }
-    static void Lab4a()
+    static void Lab5a()
     {
-        Console.WriteLine("HUNT TEST\n");
-        var o = new Orc() { Name = "Gorbag", Rage = 7 };
-        o.SayHi();
-        for (int i = 0; i < 10; i++)
+        try
         {
-            o.Hunt();
-            o.SayHi();
-        }
+            var r1 = new Rectangle(1, 3, 5, 7);
+            Console.WriteLine($"{r1}");
 
-        Console.WriteLine("\nSING TEST\n");
-        var e = new Elf("Legolas", agility: 2);
-        e.SayHi();
-        for (int i = 0; i < 10; i++)
-        {
-            e.Sing();
-            e.SayHi();
-        }
+            var p1 = new Point(3, 4);
+            var p2 = new Point(7, 8);
+            var r2 = new Rectangle(p1, p2);
+            Console.WriteLine($"{r2}");
 
-        Console.WriteLine("\nPOWER TEST\n");
-        Creature[] creatures = {
-        o,
-        e,
-        new Orc("Morgash", 3, 8),
-        new Elf("Elandor", 5, 3)
-    };
-        foreach (Creature creature in creatures)
+            var point1 = new Point(3, 4);
+            var point2 = new Point(9, 10);
+            Console.WriteLine($"Punkt {point1} jest w prostokącie: {r1.Contains(point1)}");
+            Console.WriteLine($"Punkt {point2} jest w prostokącie: {r1.Contains(point2)}");
+
+            var r3 = new Rectangle(2, 2, 2, 2);
+        }
+        catch (ArgumentException ex)
         {
-            Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
+            Console.WriteLine($"Error: {ex.Message}");
         }
     }
 
-    static void Lab4b()
+    static void Lab5b()
     {
-        object[] myObjects = {
-        new Animals() { Description = "dogs"},
-        new Birds { Description = "  eagles ", Size = 10 },
-        new Elf("e", 15, -3),
-        new Orc("morgash", 6, 4)
-    };
-        Console.WriteLine("\nMy objects:");
-        foreach (var o in myObjects) Console.WriteLine(o);
-        /*
-            My objects:
-            ANIMALS: Dogs <3>
-            BIRDS: Eagles (fly+) <10>
-            ELF: E## [10][0]
-            ORC: Morgash [6][4]
-        */
+  
     }
 }
