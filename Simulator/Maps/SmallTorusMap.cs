14d1cbfe8a6321d4bd8e5fbd4479552236192354
Darya <225284@student.uek.krakow.pl>
2024-11-13 19:09:49 +0100

    TDD

---
+++ Simulator/Maps/SmallTorusMap.cs
@@ -0,0 +1,42 @@
+﻿namespace Simulator.Maps;
+
+public class SmallTorusMap : Map
+{
+    public readonly int Size;
+
+    public SmallTorusMap(int size)
+    {
+        if (size < 5 || size > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(size), "Size must be between 5 and 20");
+        }
+        Size = size;
+    }
+
+    public override bool Exist(Point p)
+    {
+        return p.X >= 0 && p.Y >= 0 && p.X < Size && p.Y < Size;
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        Point nextPoint = p.Next(d);
+
+        int newX = (nextPoint.X + Size) % Size;
+        int newY = (nextPoint.Y + Size) % Size;
+
+        return new Point(newX, newY);
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point nextDiagonalPoint = p.NextDiagonal(d);
+
+        int newX = (nextDiagonalPoint.X + Size) % Size;
+        int newY = (nextDiagonalPoint.Y + Size) % Size;
+
+        return new Point(newX, newY);
+    }
+
+}
+
