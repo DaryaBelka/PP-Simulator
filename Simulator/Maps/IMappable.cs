0b0da86263a82d7a9dbc27e084c2e807a1734117
Darya <225284@student.uek.krakow.pl>
2024-12-17 21:28:17 +0100

    Historia-2

--- Simulator/Maps/IMappable.cs
+++ Simulator/Maps/IMappable.cs
@@ -3,6 +3,8 @@
 public interface IMappable
 {
     public char Symbol { get; } // umowny symbol obiektu
+    Point Position { get; }
+
     void Go(Direction direction);
     void InitMapAndPosition(Map map, Point point);
     public string ToString();
